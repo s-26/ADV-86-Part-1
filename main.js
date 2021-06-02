b27c8bc35158ab64edd2c97cc06508d3214607a1
s-26 <79651847+s-26@users.noreply.github.com>
2021-06-02 10:46:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var canvas=new fabric.Canvas("my_canvas");
+player_x=10;
+player_y=10;
+block_width=30;
+block_height=30;
+var player_object="";
+var block_object="";
+function player_update(){
+    fabric.Image.fromURL("player.png", function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+function new_image(get_image){
+    fabric.Image.fromURL("get_image", function(Img){
+        block_object=Img;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_object);
+    });
+}
